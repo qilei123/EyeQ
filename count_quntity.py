6da4efafa0869c6f72ce202c14a6a02f4b5f0b3b
qilei123 <qileimail123@gmail.com>
2020-11-30 23:58:55 -0500

    add cp

--- count_quntity.py
+++ count_quntity.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import os
 
-def cp_reject(csv_file):
-
+def cp_quality(csv_file,quality_label=0):
+    quality_des = ['good','usable','reject']
     if "test" in csv_file:
         dataset = "test"
     elif "train" in csv_file:
@@ -16,8 +16,11 @@ def cp_reject(csv_file):
     print(csv_records.quality.value_counts())
 
     for file_name,label in zip(csv_records.image,csv_records.quality):
-        if label==2:
-            command = "cp /data0/qilei_chen/AI_EYE/kaggle_data/"+dataset+"_/"+file_name+" /data1/qilei_chen/DATA/DB_NATURAL/data3/rejects/"
+        if label==quality_label:
+            command = "cp /data0/qilei_chen/AI_EYE/kaggle_data/"+dataset+"_/"+file_name+" /data1/qilei_chen/DATA/DB_NATURAL/data3/"+quality_des[int(quality_label)]+"/"
             os.system(command)
-cp_reject("data/Label_EyeQ_train.csv")
-cp_reject("data/Label_EyeQ_test.csv")
+cp_quality("data/Label_EyeQ_train.csv")
+cp_quality("data/Label_EyeQ_test.csv")
+
+cp_quality("data/Label_EyeQ_train.csv",1)
+cp_quality("data/Label_EyeQ_test.csv",1)
