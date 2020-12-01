16006098864e3ae918e79cc77acad0b0d4ebb8ba
qilei123 <qileimail123@gmail.com>
2020-11-30 23:43:57 -0500

    add cp

--- count_quntity.py
+++ count_quntity.py
@@ -4,9 +4,9 @@ import os
 def cp_reject(csv_file):
 
     if "test" in csv_file:
-        dataset = "train"
-    elif "train" in csv_file:
         dataset = "test"
+    elif "train" in csv_file:
+        dataset = "train"
     else:
         print("No Action!")
         return 
@@ -17,7 +17,7 @@ def cp_reject(csv_file):
 
     for file_name,label in zip(csv_records.image,csv_records.quality):
         if label==2:
-            command = "cp /data0/qilei_chen/AI_EYE/kaggle_data/"+dataset+"_ "+file_name+" /data1/qilei_chen/DATA/DB_NATURAL/data3/rejects/"
+            command = "cp /data0/qilei_chen/AI_EYE/kaggle_data/"+dataset+"_/"+file_name+" /data1/qilei_chen/DATA/DB_NATURAL/data3/rejects/"
             os.system(command)
 cp_reject("data/Label_EyeQ_train.csv")
 cp_reject("data/Label_EyeQ_test.csv")
